f64d7db9475990fe0da28812dfcf90d3442c2bd0
mahnoor510 <mahnoorsajid04@gmail.com>
2021-11-13 18:44:53 +0500

    activities adeed

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,12 +12,42 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Intent intent;
+    Intent intent1;
+    Intent intent2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn=findViewById(R.id.btngit);
+        Button btn1=findViewById(R.id.btntest);
+        Button btn2=findViewById(R.id.btnpractice);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri web= Uri.parse("https://github.com/mahnoor510/islamicAPP.git");
+                intent=new Intent(Intent.ACTION_VIEW,web);
+                startActivity(intent);
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent1=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent1);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent2=new Intent(MainActivity.this,MainActivity3.class);
+                startActivity(intent2);
+            }
+        });
+
 
 
     }
